7b329bf80c1d4e282847189310603a0a517857d1
Seba Navarro <miguelsebanavarro@gmail.com>
2022-03-19 18:32:53 -0300

    first commit

---
+++ WordFinder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordFinder
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var matrix = new List<string>
+            {
+                "abcdcczzyy",
+                "fgwiohzztu",
+                "chillizzyy",
+                "pqnsdltuyy",
+                "uvdxyluzyy"
+            };
+
+            var wordFinder = new WordFinder(matrix);
+
+            var wordsToFind = new List<string> {"cold", "chill", "wind", "snow", "tu"};
+
+            var foundWords =  wordFinder.Find(wordsToFind);
+
+            foreach (var foundWord in foundWords) {
+                Console.WriteLine(foundWord);
+            }
+        }
+    }
+}
---
+++ WordFinder/WordFinder.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordFinder
+{
+    public class WordFinder
+    {
+        private  IEnumerable<string> _matrix;
+        public WordFinder(IEnumerable<string> matrix) {
+            _matrix = matrix;
+            AddColumnStreams(matrix); // add columnStreams into already existent rowStreams
+        }
+
+        private void AddColumnStreams(IEnumerable<string> matrix)
+        {
+            var columnStreams = new string[] { };
+            Array.Resize(ref columnStreams, matrix.First().Length);
+
+            foreach (var rowStream in matrix)
+            {
+                var streamCharArray = rowStream.ToCharArray();
+                for (var i = 0; i < rowStream.Length; i++)
+                {
+                    columnStreams[i] += streamCharArray[i];
+                }
+            }
+
+            var newMatrix = matrix.ToList();
+            newMatrix.AddRange(columnStreams);
+
+            _matrix = newMatrix;
+        }
+
+        public IEnumerable<string> Find(IEnumerable<string> words)
+        {
+            var dict = 
+                words.ToDictionary(w => w, w => 0);
+            
+            // var stopWatch = Stopwatch.StartNew(); 
+            
+            foreach (var word in words) {
+                foreach (var stream in _matrix)
+                {
+                    if (stream.Replace(word, "").Length != stream.Length  ) {
+                        dict[word]++;
+                    }
+                }
+            } 
+            // Console.WriteLine("foreach loop execution time = {0} seconds\n", stopWatch.Elapsed.TotalSeconds);  
+
+            // var buffer = new ConcurrentQueue<string>();
+            // Parallel.ForEach(words, word =>
+            // {
+            //     foreach (var stream in _matrix)
+            //     {
+            //         if (stream.Replace(word, "").Length != stream.Length)
+            //         {
+            //             buffer.Enqueue(word);
+            //             //dict[word]++;
+            //         }
+            //     }
+            // });
+            //
+            // while (buffer.TryDequeue(out var word)) {
+            //     dict[word]++;
+            // }
+            //
+            // Console.WriteLine("Parallel foreach loop execution time = {0} seconds\n", stopWatch.Elapsed.TotalSeconds);
+
+            return dict.OrderByDescending(key => key.Value).Take(10).Select(kv=> kv.Key).ToList();
+        }
+    }
+}
